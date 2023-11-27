c0bcab6110682ebc3e47897db85c672ae01c3ac4
Laz <git@laz.rs>
2023-11-27 15:57:34 -0800

    Initial commit

---
+++ load.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadComponent(".header", "header.html");
+  await loadComponent(".footer", "footer.html");
+  setActiveLink();
+});
+
+async function loadComponent(selector, path) {
+  const response = await fetch(path);
+  const text = await response.text();
+  document.querySelector(selector).innerHTML = text;
+}
+
+function setActiveLink() {
+  const currentPage = window.location.pathname.split("/").pop();
+  let activeLinkId = "nav-home";
+
+  switch (currentPage) {
+    case "press.html":
+      activeLinkId = "nav-press";
+      break;
+  }
+
+  const activeLink = document.querySelector(`#${activeLinkId}`);
+  console.log(activeLink);
+  activeLink.classList.add("pages__link--active");
+}
